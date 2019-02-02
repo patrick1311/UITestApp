0c9a8221889af58ef9dd752ff38e0a9762251612
Patrick Quach <patrickquach96@gmail.com>
2019-02-02 15:51:12 -0800

    Added Credits Screen and link button back and forth

---
+++ Assets/CreditsUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsUI : MonoBehaviour
+{
+    public void LoadHome()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+}
---
+++ Assets/HomeUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
